0f552e77edf2932f96ff417009934e76d1c2f952
Pablo Naveira López <pablonaveiralopez@gmail.com>
2024-06-29 10:30:32 +0200

    feat: change tailwind conf

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  content: ["./src/**/*.{html,js, ts, tsx}"],
+  content: ["./src/**/*.{html,js,ts,tsx}"],
   darkMode: false,
   theme: {
     extend: {},
